bc1c882c0ec72ad6ef22960f61d92aac6b3e3883
omniacommuniasunt <std150473@ac.eap.gr>
2024-07-08 16:56:30 +0300

    Updated files variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,7 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
-
+# Just to commit again
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
